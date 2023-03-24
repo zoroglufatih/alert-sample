ce9b95bee014d0a3b3f5bb24a6dcf4174a3fd333
zoroglufatih <fatihzorogluu@gmail.com>
2023-03-24 18:01:17 +0300

    Initial Commit

---
+++ AlertProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlertProject
+//
+//  Created by Fatih Zoroğlu on 24.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
